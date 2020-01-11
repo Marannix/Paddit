3821c43596d63609a01ac0240123734585c8d50e
Josh <onabanjotobi@gmail.com>
2020-01-11 15:11:17 +0000

    Display full screen loading dialog while data is being fetched

--- app/src/main/java/com/example/paddit/activity/DashboardActivity.kt
+++ app/src/main/java/com/example/paddit/activity/DashboardActivity.kt
@@ -1,27 +1,28 @@
 package com.example.paddit.activity
 
+import android.app.Dialog
 import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.paddit.R
+import com.example.paddit.dialog.FullscreenLoadingDialog
 import com.example.paddit.fragment.DashboardFragment
 import com.example.paddit.model.PostResponse
 import com.example.paddit.model.UserResponse
-import com.example.paddit.repository.PostRepository
 import com.example.paddit.viewmodel.PostViewModel
-import javax.inject.Inject
 
 class DashboardActivity : BaseActivity() {
 
-    @Inject
-    lateinit var postRepository: PostRepository
-
     private lateinit var viewModel: PostViewModel
+    private lateinit var loadingDialog: Dialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        loadingDialog = FullscreenLoadingDialog(this).apply {
+            setCanceledOnTouchOutside(false)
+        }
         viewModel = this.let { ViewModelProviders.of(it, viewModelFactory).get(PostViewModel::class.java) }
         viewModel.start()
 
@@ -29,17 +30,21 @@ class DashboardActivity : BaseActivity() {
             when (it) {
                 //Remove empty
                 PostViewModel.ViewState.Empty -> {
+                    loadingDialog.hide()
                     Log.d("look chief", "I'm empty")
                 }
                 PostViewModel.ViewState.Loading -> {
+                    loadingDialog.show()
                     Log.d("look chief", "I'm loading")
                 }
                 is PostViewModel.ViewState.Content -> {
+                    loadingDialog.hide()
                     initVenueFragment(it.post, it.users)
                     Log.d("look chief - post", it.post.toString())
                     Log.d("look chief - user", it.users.toString())
                 }
                 is PostViewModel.ViewState.Error -> {
+                    loadingDialog.hide()
                     Log.d("look chief error", it.error)
                 }
             }
