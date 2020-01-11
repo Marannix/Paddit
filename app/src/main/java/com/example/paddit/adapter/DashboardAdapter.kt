8aa5328e56506d125d148350917dff418b692d7b
Josh <onabanjotobi@gmail.com>
2020-01-11 15:31:06 +0000

    Created an adapter to display the user and their corresponding posts

---
+++ app/src/main/java/com/example/paddit/adapter/DashboardAdapter.kt
@@ -0,0 +1,57 @@
+package com.example.paddit.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.paddit.R
+import com.example.paddit.model.PostResponse
+import com.example.paddit.model.UserResponse
+import kotlinx.android.synthetic.main.dashboard_items.view.*
+
+class DashboardAdapter : RecyclerView.Adapter<DashboardAdapter.ViewHolder>() {
+
+    private var posts: List<PostResponse> = emptyList()
+    private var users: List<UserResponse> = emptyList()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.dashboard_items, parent, false))
+    }
+
+    override fun getItemCount(): Int {
+        return if (posts.size > 10) {
+            10
+        } else {
+            posts.size
+        }
+    }
+
+    fun setData(posts: List<PostResponse>, users: List<UserResponse>) {
+        this.posts = posts
+        this.users = users
+        this.notifyDataSetChanged()
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        if (posts.isNotEmpty()) {
+            holder.bind(posts[position], users)
+        }
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        fun bind(
+            posts: PostResponse,
+            users: List<UserResponse>
+        ) {
+
+           for (i in 0..users.size) {
+               if (users[i].id == posts.userId) {
+                   itemView.username.text = users[i].username
+                   break
+               }
+           }
+//            itemView.username.text = posts.id.toString()
+        }
+    }
+
+}
--- app/src/main/java/com/example/paddit/fragment/DashboardFragment.kt
+++ app/src/main/java/com/example/paddit/fragment/DashboardFragment.kt
@@ -1,11 +1,13 @@
 package com.example.paddit.fragment
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.paddit.R
+import com.example.paddit.adapter.DashboardAdapter
 import com.example.paddit.model.PostResponse
 import com.example.paddit.model.UserResponse
 import kotlinx.android.synthetic.main.fragment_dashboard.*
@@ -13,21 +15,27 @@ import kotlinx.android.synthetic.main.fragment_dashboard.*
 
 class DashboardFragment : BaseFragment() {
 
-    companion object {
-        @JvmStatic
-        fun newInstance() = DashboardFragment()
-    }
+    private val adapter = DashboardAdapter()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        Log.d("created", "view created")
         return inflater.inflate(R.layout.fragment_dashboard, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        createAdapter()
     }
 
     fun display(
         posts: List<PostResponse>,
         users: List<UserResponse>
     ) {
-        textview.text = posts[4].userId.toString()
+        adapter.setData(posts, users)
+    }
+
+    private fun createAdapter() {
+        dashboardRecyclerView.layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+        dashboardRecyclerView.adapter = adapter
     }
 
 }
