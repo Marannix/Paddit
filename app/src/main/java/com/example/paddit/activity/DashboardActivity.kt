62b5b657c5e12fcf6290dbace030bfcae8210fce
Josh <onabanjotobi@gmail.com>
2020-01-11 18:00:48 +0000

    Added an error layout when an error occurs.

--- app/src/main/java/com/example/paddit/activity/DashboardActivity.kt
+++ app/src/main/java/com/example/paddit/activity/DashboardActivity.kt
@@ -3,6 +3,7 @@ package com.example.paddit.activity
 import android.app.Dialog
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.paddit.R
@@ -11,6 +12,8 @@ import com.example.paddit.fragment.DashboardFragment
 import com.example.paddit.model.PostResponse
 import com.example.paddit.model.UserResponse
 import com.example.paddit.viewmodel.PostViewModel
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.error_layout.*
 
 class DashboardActivity : BaseActivity() {
 
@@ -25,40 +28,59 @@ class DashboardActivity : BaseActivity() {
         }
         viewModel = this.let { ViewModelProviders.of(it, viewModelFactory).get(PostViewModel::class.java) }
         viewModel.start()
-
         viewModel.livedata.observe(this, Observer {
             when (it) {
                 PostViewModel.ViewState.Empty -> {
                     loadingDialog.hide()
-                    Log.d("look chief", "I'm empty")
                 }
                 PostViewModel.ViewState.Loading -> {
                     loadingDialog.show()
+                    showErrorLayout(false)
                     Log.d("look chief", "I'm loading")
                 }
                 is PostViewModel.ViewState.Content -> {
                     loadingDialog.hide()
                     initVenueFragment(it.post, it.users)
+                    showErrorLayout(false)
                     Log.d("look chief - post", it.post.toString())
                     Log.d("look chief - user", it.users.toString())
                 }
                 is PostViewModel.ViewState.Error -> {
                     loadingDialog.hide()
+                    showErrorLayout(true)
                     Log.d("look chief error", it.error)
                 }
             }
         })
     }
 
+    private fun showErrorLayout(show: Boolean) {
+        when {
+            show -> {
+                errorLayout.visibility = View.VISIBLE
+                restartButton.setOnClickListener {
+                    viewModel.start()
+                }
+            }
+            else -> errorLayout.visibility = View.INVISIBLE
+        }
+
+    }
+
     private fun initVenueFragment(
         posts: List<PostResponse>,
         users: List<UserResponse>
     ) {
-
         supportFragmentManager.createFragment(
             fragmentId = R.id.fragmentContainer,
             factory = { DashboardFragment() }
         ).display(posts, users)
     }
 
+    // Added due to android.view.WindowLeaked
+    // When I close the activity the loading progress is triggered which is odd
+    override fun onPause() {
+        loadingDialog.dismiss()
+        super.onPause()
+    }
 }
