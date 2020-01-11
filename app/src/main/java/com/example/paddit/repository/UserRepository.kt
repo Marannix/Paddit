a840a1fc995af441733ef251371eddbaf71c2533
Josh <onabanjotobi@gmail.com>
2020-01-11 13:49:44 +0000

    Store and retrieve user from the database

--- app/src/main/java/com/example/paddit/repository/UserRepository.kt
+++ app/src/main/java/com/example/paddit/repository/UserRepository.kt
@@ -1,21 +1,39 @@
 package com.example.paddit.repository
 
 import com.example.paddit.api.UserApi
+import com.example.paddit.data.UserDao
 import com.example.paddit.model.UserResponse
 import io.reactivex.Single
 import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 class UserRepository @Inject constructor(
-    private val userApi: UserApi
+    private val userApi: UserApi,
+    private val userDao: UserDao
 ) {
 
     fun getUsers(): Single<List<UserResponse>> {
-       return getUsersFromApi()
+       return retrieveUsersFromDb()
+    }
+
+    private fun retrieveUsersFromDb() : Single<List<UserResponse>> {
+        return userDao.getUsers().
+            flatMap { listOfUsers ->
+                if (listOfUsers.isEmpty()) {
+                    // TODO: Same issue with posts, need to find a way to update the database
+                    // Maybe I should clear the database or something?
+                    getUsersFromApi()
+                } else {
+                    Single.just(listOfUsers)
+                }
+            }
     }
 
     private fun getUsersFromApi(): Single<List<UserResponse>> {
-        return userApi.getUsers().subscribeOn(Schedulers.io())
-
+        return userApi.getUsers().doOnSuccess {listOfUsers ->
+          userDao.insertUsers(listOfUsers)
+        }.subscribeOn(Schedulers.io())
     }
+
+
 }
